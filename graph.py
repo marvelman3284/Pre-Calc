d4741d946e6499b4bb8595f76f7f2dd9a53f1c27
marvelman3284 <jedimaster3284@gmail.com>
2022-10-12 23:33:36 -0400

    add doc comments, and graph_eq function

--- graph.py
+++ graph.py
@@ -10,23 +10,49 @@ def graph(
     x: list[int],
     y: list[int],
     title: Optional[str] = None,
-    x_ticks: Optional[str] = None,
-    y_ticks: Optional[str] = None,
+    x_ticks: Optional[list[str]] = None,
+    y_ticks: Optional[list[str]] = None,
 ) -> None:
+    """
+    Graphs a set of (x, y) coordinates
+
+    Args:
+        x (list[int]): a list of all the x coordinates
+        y (list[int]): a list of all the y coordinates
+        title (Optional[str]): the title of the figure
+        x_ticks (Optional[list[str]]): a list of strings to use as the x tick lables
+        y_ticks (Optional[list[str]]): a list of strings to use as the y tick lables
+
+    Returns:
+        None
+    """
+
     ax = plt.subplot()
-    plt.plot(x, y, marker='h')
+    plt.plot(x, y, marker="h")
 
     if title:
         plt.title(title)
     if x_ticks:
-        ax.set_xticks(x_ticks)
+        ax.set_xticklabels(x_ticks)
     if y_ticks:
-        ax.set_yticks(y_ticks)
+        ax.set_yticklabels(y_ticks)
 
     plt.show()
 
 
 def clean_equations(eq: str):
+    """
+    Return a sympified equation.
+
+    Given a string turn it into a equation that is usable by sympy.
+
+    Args:
+        eq (str): the equation to be sympified
+
+    Returns:
+        Any: a sympify equation
+    """
+
     try:
         return sp.sympify(eq)
     except ValueError:
@@ -35,13 +61,34 @@ def clean_equations(eq: str):
         return clean_equations(new_eq)
 
 
-def graph_eq(eq: str, num_points: int):
+def graph_eq(
+    eq: str,
+    num_points: list[int],
+    title: Optional[str] = "Figure 1",
+    x_ticks: Optional[list[str]] = None,
+    y_ticks: Optional[list[str]] = None,
+):
+    """
+    Graph an equation.
+
+    Given an equation and an amount of points, graph the equation
+
+    Args:
+        eq (str): an equation
+        num_points (list[int]): a list containing the start and stop of the range
+        title (Optional[str]): the title of the figure, defaults to "figure 1"
+        x_ticks (Optional[list[str]]): a list of strings to use as the x tick lables, defaults to None
+        y_ticks (Optional[list[str]]): a list of strings to use as the y tick lables, defaults to None
+
+    Returns:
+        None: prints a graph
+    """
+
     eq = clean_equations(eq)
 
-    x = [i for i in range(num_points)]
-    y = [eq.subs(sp.symbols("x"), i) for i in range(num_points)]  # type:ignore
+    x = [i for i in range(num_points[0], num_points[1] + 1)]
+    y = [
+        eq.subs(sp.symbols("x"), i) for i in range(num_points[0], num_points[1] + 1)  # type: ignore
+    ]
 
-    graph(x, y)
-
-
-graph_eq("x**2", 5)
+    graph(x, y, title, x_ticks, y_ticks)
